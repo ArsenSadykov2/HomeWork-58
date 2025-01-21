b03642666699c189f10532655750c16aaba86967
Sadykov Arsen <LoCo10291998@gmail.com>
2025-01-21 17:35:23 +0600

    Second Commit. Added buttons to the modal window. And clicking the Close button made the closing function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import Modal from "./components/Modal/Modal.tsx";
+import {useState} from "react";
 
 const App = () => {
+    const [showModal, setShowModal] = useState<boolean>(false);
 
-
+    const addButtons = [
+        {type: 'primary', label: 'Continue', onClick: () => console.log('Continue')},
+        {type: 'danger', label: 'Close', onClick: () => setShowModal(prevShow => !prevShow)},
+    ]
 
     return (
-        <div className="container">
-
-        </div>
+        <>
+            <Modal
+                show={showModal}
+                closeModal={addButtons[1].onClick}
+                title="Some kinda modal title"
+                addButtons={addButtons}
+            >
+                <p>This is modal content</p>
+            </Modal>
+            <button className="btn btn-primary" onClick={() => setShowModal(true)}>Open Modal</button>
+        </>
     )
 };
 
